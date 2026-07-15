23fb5ded1a63af84dcd40e6f52f364d9d8e46202
tmigueis-belodigital <87386258+Migueis11@users.noreply.github.com>
2026-07-15 12:14:28 +0100

    clients-invoice-sidebar

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,14 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::view('clients', 'clients')
+    ->middleware(['auth', 'verified'])
+    ->name('clients');
+
+Route::view('invoices', 'invoices')
+    ->middleware(['auth', 'verified'])
+    ->name('invoices');
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
